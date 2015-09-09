04debcd32f3317c9be95d5c00c66891a597e56dd
Tyler Garlick <tjgarlick@gmail.com>
2015-09-09 07:17:30 -0600

    fixes to travis ci

--- test/projection.tests.js
+++ test/projection.tests.js
@@ -4,27 +4,27 @@ let expect = require('chai').expect;
 let Projection = require('../lib/projector');
 let Lazy = require('lazy.js');
 
-describe('Projection', function () {
+describe('Projection', function() {
 
-  it('should setup correctly', function () {
+  it('should setup correctly', function() {
     var projection = new Projection();
     expect(projection).to.be.ok;
   });
 
-  describe('#only', function () {
+  describe('#only', function() {
 
-    it('should grab only the properties', function () {
+    it('should grab only the properties', function() {
       const source = { here: '', is: '', my: 1, thing: null };
       let result = Projection.only(source, ['is']);
       expect(result.is).to.be.empty;
     });
 
-    it('should return a naked object', function () {
+    it('should return a naked object', function() {
       const source = { here: '', is: '', my: 1, thing: null };
       expect(Projection.only(source, [])).to.be.eql({});
     });
 
-    it('should return the full object', function () {
+    it('should return the full object', function() {
       const source = { here: '', is: '', my: 1, thing: null };
       const keys = Lazy(source).keys().toArray();
       expect(Projection.only(source, keys)).to.be.eql(source);
@@ -32,35 +32,24 @@ describe('Projection', function () {
 
   });
 
-  describe('#remove', function () {
+  describe('#remove', function() {
 
-    it('should remove properties', function () {
+    it('should remove properties', function() {
       const source = { here: '', is: '', my: 1, thing: null };
       let result = Projection.without(source, ['here', 'my', 'thing']);
       expect(result.is).to.be.empty;
     });
 
-    it('should return a naked object', function () {
+    it('should return a naked object', function() {
       const source = { here: '', is: '', my: 1, thing: null };
       expect(Projection.without(source, [])).to.be.eql(source);
     });
 
-    it('should return the full object', function () {
+    it('should return the full object', function() {
       const source = { here: '', is: '', my: 1, thing: null };
       expect(Projection.without(source, ['here', 'is', 'my'])).to.be.eql({ thing: null });
     });
 
   });
-
-  describe('#translate', function () {
-
-    it('should be able to translate', function () {
-      const source = { here: '', is: '', my: 1, thing: null };
-      let result = Projection.transform(source, { 'blah': 'thing' });
-      expect(result).to.be.eql({ blah: null });
-    });
-
-  });
-
 
 });
